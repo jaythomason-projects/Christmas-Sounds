8f2a97e6609451569ec492fda76f6a4aeeab95cb
jaythomason-projects <jaythomason.projects@outlook.com>
2023-12-21 17:47:37 +1100

    fix: if name not in greetings, modal is not displayed Issue: If the name param is not found in greetings.js, fullName is not defined and modal will not display, page will not load Cause: this situation wasn't handled Fix: if greetings = none, set fullname to name param

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { greetings } from './greetings.js';
 
 const Modal = ({ name, setShowModal }) => {
-    const properCaseName = name.charAt(0).toUpperCase() + name.slice(1);
-    const message = `
-    Wishing you a very merry Christmas and a happy new year!
-    Take a moment to unwind and embrace the joyful spirit of the holiday season with these soothing Christmas sounds.
-    May they provide a delightful ambiance as you get ready for Christmas day.`;
+    const greeting = greetings.find(greeting => greeting.name === name);
+    const fullName = greeting ? greeting.fullName : name;
+    const defaultMessage = `
+        Wishing you a very merry Christmas and a happy new year!
+        Take a moment to unwind and embrace the joyful spirit of the holiday season with these soothing Christmas sounds.
+        May they provide a delightful ambiance as you get ready for Christmas day.`;
+    const message = greeting && greeting.message !== '' ? greeting.message : defaultMessage;
 
     return (
         <>
             <div className="overlay"></div>
             <dialog className="modal" open>
-                <div className="modal__content">
-                    <h2 className="modal__title">Merry Christmas, {properCaseName}!</h2>
+                <div>
+                    <h2>Merry Christmas, {fullName}!</h2>
                     <p>{message}</p>
-                    <button
-                        className="modal__button"
-                        onClick={() => setShowModal(false)}
-                    >
-                        Go
-                    </button>
+                    <button onClick={() => setShowModal(false)}>Go</button>
                 </div>
             </dialog>
         </>
