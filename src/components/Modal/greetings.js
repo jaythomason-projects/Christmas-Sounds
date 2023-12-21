39038eef685692d1b63bdb4e69391c3e1ae42e4a
jaythomason-projects <jaythomason.projects@outlook.com>
2023-12-21 17:47:51 +1100

    Add greetings data and update soundboard layout

---
+++ src/components/Modal/greetings.js
@@ -0,0 +1,40 @@
+export const greetings = [
+    {
+        name: 'Jay',
+        fullName: 'Jay R. and Emily',
+        message: '',
+    },
+    {
+        name: 'Sophie',
+        fullName: 'Sophie',
+        message: '',
+    },
+    {
+        name: 'Alice',
+        fullName: 'Alice',
+        message: '',
+    },
+    {
+        name: 'Alicia',
+        fullName: 'Alicia & Riley',
+        message: '',
+    },
+    {
+        name: 'Branden',
+        fullName: 'Branden & Yana',
+        message: 
+        `Wishing you a very merry Christmas and a happy new year! (sorry if I spelt your names wrong!)
+        Take a moment to unwind and embrace the joyful spirit of the holiday season with these soothing Christmas sounds.
+        May they provide a delightful ambiance as you get ready for Christmas day.`,
+    },
+    {
+        name: 'Alicia',
+        fullName: 'Alicia & Riley',
+        message: '',
+    },
+    {
+        name: 'Alex',
+        fullName: 'Alexes',
+        message: '',
+    },
+]
